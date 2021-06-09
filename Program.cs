116ab7ebded356ceca2693e10ff3020b21c3a62b
Mark Anderson <markanderson8162@gmail.com>
2021-06-09 10:29:22 -0500

    new Hello World project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Greetings Planet!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Greetings {name}. What is your quest?");
+            var quest = Console.ReadLine();
+            Console.WriteLine($"Your quest is {quest}.");
+            Console.WriteLine("What is the airspeed velocity of an unladen swallow?");
+            var swallow = Console.ReadLine();
+            Console.WriteLine($"Yes, {swallow} is correct!");
+        }
+    }
+}
